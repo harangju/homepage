fb526b94878ebf29aafa1f15e8350a9319f702df
Harang Ju <harang@mit.edu>
2023-11-07 10:34:37 -0500

    icon colors

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,21 +6,20 @@ export default function Footer() {
     <footer className="fixed bottom-0 w-full z-20 max-w-2xl py-2 px-4 border-t">
       <div className="flex justify-between w-full">
 
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <span className="text-sm text-gray-900 sm:text-center 700:text-gray-400">
           © 2023 <a href="https://harangju.com/" className="hover:underline">Harang Ju</a>
         </span>
         
         <div className="flex items-center justify-center space-x-4">
-          <a href="https://x.com/harangju/" target="_blank" rel="noreferrer">
-            <X className="w-4 h-4 text-gray-500 hover:text-gray-400 transition-all duration-100" />
-          </a>
           <a href="https://scholar.google.com/citations?user=dDNw63AAAAAJ&hl=en" target="_blank" rel="noreferrer">
-            <GraduationCap className="w-5 h-5 text-gray-500 hover:text-gray-400 transition-all duration-100" />
+            <GraduationCap className="w-5 h-5 text-gray-900 hover:text-gray-700 transition-all duration-100" />
+          </a>
+          <a href="https://x.com/harangju/" target="_blank" rel="noreferrer">
+            <X className="w-5 h-5 text-gray-900 hover:text-gray-700 transition-all duration-100" />
           </a>
           <a href="https://www.linkedin.com/in/harangju/" target="_blank" rel="noreferrer">
-            <LinkedIn className="w-5 h-5 text-gray-500 hover:text-gray-400 transition-all duration-100" />
+            <LinkedIn className="w-5 h-5 text-gray-900 hover:text-gray-700 transition-all duration-100" />
           </a>
-
         </div>
 
       </div>
