dac6ac37617cbd191582daa01cebfc542901496b
Harang Ju <harang@mit.edu>
2023-11-09 10:05:22 -0500

    Update footer styling and links

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,27 +4,27 @@ import { Instagram, LinkedIn, X } from "@/components/icons";
 export default function Footer({ className } : { className?: string }) {
   return (
     <footer className={`flex w-full z-20 py-2 border-t ${className}`}>
-      <div className="flex justify-between w-full">
+      <div className="flex justify-between w-full text-slate-900 dark:text-slate-50">
 
-        <span className="text-sm text-gray-900 sm:text-center 700:text-gray-400">
+        <span className="text-sm">
           © 2023 <a href="https://harangju.com/" className="hover:underline">Harang Ju</a>
         </span>
         
-        <div className="flex items-center justify-center space-x-3">
+        <div className="flex items-center justify-center space-x-3 hover:text-slate-700 transition-all duration-100">
           <a href="/Harang Ju CV.pdf" target="_blank" rel="noreferrer">
-            <FileText className="w-5 h-5 -mr-[3px] text-gray-900 hover:text-gray-700 transition-all duration-100" />
+            <FileText className="w-5 h-5 -mr-[3px]" />
           </a>
           <a href="https://scholar.google.com/citations?user=dDNw63AAAAAJ&hl=en" target="_blank" rel="noreferrer">
-            <GraduationCap className="w-5 h-5 text-gray-900 hover:text-gray-700 transition-all duration-100" />
+            <GraduationCap className="w-5 h-5" />
           </a>
           <a href="https://www.instagram.com/harangju/" target="_blank" rel="noreferrer">
-            <Instagram className="w-5 h-5 text-gray-900 hover:text-gray-700 transition-all duration-100" />
+            <Instagram className="w-5 h-5" />
           </a>
           <a href="https://x.com/harangju/" target="_blank" rel="noreferrer">
-            <X className="w-5 h-5 text-gray-900 hover:text-gray-700 transition-all duration-100" />
+            <X className="w-5 h-5" />
           </a>
           <a href="https://www.linkedin.com/in/harangju/" target="_blank" rel="noreferrer">
-            <LinkedIn className="w-5 h-5 text-gray-900 hover:text-gray-700 transition-all duration-100" />
+            <LinkedIn className="w-5 h-5" />
           </a>
         </div>
 
